aeb48c49e6eda6903012f4fa132949dcd45292df
Manos Fountoulakis <it2024109@hua.gr>
2025-12-08 22:57:53 +0200

    Addeed laps and Tracks

---
+++ FitnessManager/src/main/java/gr/hua/dit/fitnessmanager/Laps.java
@@ -0,0 +1,9 @@
+package gr.hua.dit.fitnessmanager;
+
+public class Laps {
+    private Tracks tracks;
+
+    public Laps(Tracks tracks) {
+        this.tracks = tracks;
+    }
+}
---
+++ FitnessManager/src/main/java/gr/hua/dit/fitnessmanager/Tracks.java
@@ -0,0 +1,7 @@
+package gr.hua.dit.fitnessmanager;
+import java.time.LocalDateTime;
+
+public class Tracks {
+    //private List<Trackpoint> trackpoints = new ArrayList<>();
+
+}
