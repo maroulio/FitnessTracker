aaa4ea16b3187ccacbe144bb1df0694df9c3b879
Manos Fountoulakis <it2024109@hua.gr>
2025-12-12 11:59:09 +0200

    Utilized laps

--- FitnessManager/src/main/java/gr/hua/dit/fitnessmanager/Laps.java
+++ FitnessManager/src/main/java/gr/hua/dit/fitnessmanager/Laps.java
@@ -1,4 +1,5 @@
 package gr.hua.dit.fitnessmanager;
+import java.time.LocalDateTime;
 
 public class Laps {
     private Tracks tracks;
@@ -7,5 +8,26 @@ public class Laps {
         this.tracks = tracks;
     }
 
+    public LocalDateTime getStartTime() {
+        return tracks.StartTime;
+    }
+    public int getAHR() {
+        return tracks.AHR();
+    }
+    public int getAHR() {
+        return tracks.AHR();
+    }
+    public int getMHR() {
+        return tracks.MHR();
+    }
+    public double getDistanceMeters() {
+        return tracks.distanceMeters();
+    }
+    public double getTimeSeconds() {
+        return tracks.timeSeconds();
+    }
+    public double getAverageSpeed) {
+        return tracks.averageSpeed();
+    }
 
 }
