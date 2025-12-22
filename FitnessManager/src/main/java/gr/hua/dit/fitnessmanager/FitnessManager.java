df3d4f1af9f9575cb3a746799bcb21c4f5e64aa0
anastasiantoutsi <it2024073@hua.gr>
2025-12-22 19:08:34 +0200

    Complete README.md and fix line 25 with file[i].getAbsolutePath()

--- FitnessManager/src/main/java/gr/hua/dit/fitnessmanager/FitnessManager.java
+++ FitnessManager/src/main/java/gr/hua/dit/fitnessmanager/FitnessManager.java
@@ -22,142 +22,142 @@ public class FitnessManager {
             if (file[i].exists() && file[i].isFile()) {
                 //parser
 
-                String tcx = file[i]; //μεχρι να μου δινετε το αρχειο απο τα args
+                String tcx = file[i].getAbsolutePath(); //μεχρι να μου δινετε το αρχειο απο τα args
                 TCXParser parser = new TCXParser(tcx);
                 ArrayList<Activity> act = parser.parse();
 
-
-
-                ActivityFactory sport = new ActivityFactory();
-                Activity activity = sport.createActivity(null);
-                System.out.println("Activity: " + activity.getName());
-                System.out.println("Total Time: " + activity.getTimeSeconds() / 60 + ":" + activity.getTimeSeconds() % 60);
-                System.out.println("Total Distance: " + activity.getDistanceMeters() / 1000);
-                System.out.println("Avg Pace: " + activity.getAveragePace() + " min/km");
-                System.out.println("Avg Heart Rate: " + activity.getAHR() + " bpm");
-                System.out.println("Avg Speed: " + activity.getAverageSpeed() + " km/h");
-                if (file.length > 1) {
-                    int cal = 0;
-                    boolean f = false;
-                    for (int j = 0; j < args.length; j++) {
-                        if (args[j].startsWith("-")) {
-                            f = true;
-                            break;
-                        }
-                    }
-                    if (f) {
-                        double weight = 0;
+                for (int k = 0; k < act.toArray().length; k++) {
+                    ActivityFactory sport = new ActivityFactory();
+                    Activity activity = sport.createActivity(null);
+                    System.out.println("Activity: " + activity.getName());
+                    System.out.println("Total Time: " + activity.getTimeSeconds() / 60 + ":" + activity.getTimeSeconds() % 60);
+                    System.out.println("Total Distance: " + activity.getDistanceMeters() / 1000);
+                    System.out.println("Avg Pace: " + activity.getAveragePace() + " min/km");
+                    System.out.println("Avg Heart Rate: " + activity.getAHR() + " bpm");
+                    System.out.println("Avg Speed: " + activity.getAverageSpeed() + " km/h");
+                    if (file.length > 1) {
+                        int cal = 0;
+                        boolean f = false;
                         for (int j = 0; j < args.length; j++) {
-                            if (args[j].equals("-w")) {
-                                if (j + 1 < args.length) {
-                                    try {
-                                        weight = Double.parseDouble(args[++j]);
-                                    } catch (Exception e) { // It may be an InputMismatchException
-                                        return;
-                                    }
-                                    break;
-                                }
+                            if (args[j].startsWith("-")) {
+                                f = true;
+                                break;
                             }
                         }
-                        f = false;
-                        int age = 0;
-                        for (int j = 0; j < args.length; j++) {
-                            if (args[j].equals("-a")) {
-                                if (j + 1 < args.length) {
-                                    try {
-                                        age = Integer.parseInt(args[++j]);
-                                    } catch (Exception e) {
-                                        System.out.println("cal += activity.getCal(weight, activity.getTimeSeconds())");
-                                        f = true;
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-                        if (!f) {
-                            f = false;
-                            char gender;
+                        if (f) {
+                            double weight = 0;
                             for (int j = 0; j < args.length; j++) {
-                                if (args[j].equals("-g")) {
+                                if (args[j].equals("-w")) {
                                     if (j + 1 < args.length) {
-                                        gender = args[++j].toLowerCase().charAt(0);
-                                        if (gender == 'm' || gender == 'f') {
-                                            System.out.println("cal += activity.getCal(weight, age, gender, activity.getTimeSeconds())");
-                                        } else {
-                                            System.out.println("cal += activity.getCal(weight, activity.getTimeSeconds())");
+                                        try {
+                                            weight = Double.parseDouble(args[++j]);
+                                        } catch (Exception e) { // It may be an InputMismatchException
+                                            return;
                                         }
-                                        f = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            f = false;
+                            int age = 0;
+                            for (int j = 0; j < args.length; j++) {
+                                if (args[j].equals("-a")) {
+                                    if (j + 1 < args.length) {
+                                        try {
+                                            age = Integer.parseInt(args[++j]);
+                                        } catch (Exception e) {
+                                            System.out.println("cal += activity.getCal(weight, activity.getTimeSeconds())");
+                                            f = true;
+                                        }
                                         break;
                                     }
                                 }
                             }
                             if (!f) {
+                                f = false;
+                                char gender;
+                                for (int j = 0; j < args.length; j++) {
+                                    if (args[j].equals("-g")) {
+                                        if (j + 1 < args.length) {
+                                            gender = args[++j].toLowerCase().charAt(0);
+                                            if (gender == 'm' || gender == 'f') {
+                                                System.out.println("cal += activity.getCal(weight, age, gender, activity.getTimeSeconds())");
+                                            } else {
+                                                System.out.println("cal += activity.getCal(weight, activity.getTimeSeconds())");
+                                            }
+                                            f = true;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (!f) {
+                                    System.out.println("cal += activity.getCal(weight, activity.getTimeSeconds())");
+                                }
+                            } else {
                                 System.out.println("cal += activity.getCal(weight, activity.getTimeSeconds())");
                             }
-                        } else {
-                            System.out.println("cal += activity.getCal(weight, activity.getTimeSeconds())");
                         }
-                    }
-                    if (cal != 0) {
-                        System.out.println("Total Calories: " + cal);
-                    }
-                } else {
-                    boolean f = false;
-                    for (int j = 0; j < args.length; j++) {
-                        if (args[j].startsWith("-")) {
-                            f = true;
-                            break;
+                        if (cal != 0) {
+                            System.out.println("Total Calories: " + cal);
                         }
-                    }
-                    if (f) {
-                        double weight = 0;
+                    } else {
+                        boolean f = false;
                         for (int j = 0; j < args.length; j++) {
-                            if (args[j].equals("-w")) {
-                                if (j + 1 < args.length) {
-                                    try {
-                                        weight = Double.parseDouble(args[++j]);
-                                    } catch (Exception e) { // It may be an InputMismatchException
-                                        return;
-                                    }
-                                    break;
-                                }
-                            }
-                        }
-                        f = false;
-                        int age = 0;
-                        for (int j = 0; j < args.length; j++) {
-                            if (args[j].equals("-a")) {
-                                if (j + 1 < args.length) {
-                                    try {
-                                        age = Integer.parseInt(args[++j]);
-                                        f = true;
-                                    } catch (Exception e) {
-                                        System.out.println("Total Calories: + activity.getCal(weight, activity.getTimeSeconds())");
-                                        return;
-                                    }
-                                    break;
-                                }
+                            if (args[j].startsWith("-")) {
+                                f = true;
+                                break;
                             }
                         }
                         if (f) {
-                            char gender = ' ';
+                            double weight = 0;
                             for (int j = 0; j < args.length; j++) {
-                                if (args[j].equals("-g")) {
+                                if (args[j].equals("-w")) {
                                     if (j + 1 < args.length) {
-                                        gender = args[++j].toLowerCase().charAt(0);
-                                        if (gender == 'm' || gender == 'f') {
-                                            System.out.println("Total Calories: + activity.getCal(weight, age, gender, activity.getTimeSeconds())");
-                                        } else {
-                                            System.out.println("Total Calories: + activity.getCal(weight, activity.getTimeSeconds())");
+                                        try {
+                                            weight = Double.parseDouble(args[++j]);
+                                        } catch (Exception e) { // It may be an InputMismatchException
+                                            return;
                                         }
-                                        return;
+                                        break;
                                     }
                                 }
                             }
+                            f = false;
+                            int age = 0;
+                            for (int j = 0; j < args.length; j++) {
+                                if (args[j].equals("-a")) {
+                                    if (j + 1 < args.length) {
+                                        try {
+                                            age = Integer.parseInt(args[++j]);
+                                            f = true;
+                                        } catch (Exception e) {
+                                            System.out.println("Total Calories: + activity.getCal(weight, activity.getTimeSeconds())");
+                                            return;
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                            if (f) {
+                                char gender = ' ';
+                                for (int j = 0; j < args.length; j++) {
+                                    if (args[j].equals("-g")) {
+                                        if (j + 1 < args.length) {
+                                            gender = args[++j].toLowerCase().charAt(0);
+                                            if (gender == 'm' || gender == 'f') {
+                                                System.out.println("Total Calories: + activity.getCal(weight, age, gender, activity.getTimeSeconds())");
+                                            } else {
+                                                System.out.println("Total Calories: + activity.getCal(weight, activity.getTimeSeconds())");
+                                            }
+                                            return;
+                                        }
+                                    }
+                                }
+                                System.out.println("Total Calories: + activity.getCal(weight, activity.getTimeSeconds())");
+                                return;
+                            }
                             System.out.println("Total Calories: + activity.getCal(weight, activity.getTimeSeconds())");
-                            return;
                         }
-                        System.out.println("Total Calories: + activity.getCal(weight, activity.getTimeSeconds())");
                     }
                 }
             }
--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 TCXParser ,Trackpoints - Maria Stavrou 2024090
          ActivityFactory, laps, tracks, other, CaloryFactory .simplecalory hrccalory,userprofile, __ Manos Fountoulakis it2024109
-
-
+Activity, Cycling, FitnessManager, Running, Swimming, Walking - Anastasia Ntoutsi 2024073
