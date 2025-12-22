f97a7357e69104dda83c1baae9954f0ca3ef7ebb
Manos Fountoulakis <it2024109@hua.gr>
2025-12-22 17:50:30 +0200

    1)Changed Laps part4

--- FitnessManager/src/main/java/gr/hua/dit/fitnessmanager/Laps.java
+++ FitnessManager/src/main/java/gr/hua/dit/fitnessmanager/Laps.java
@@ -25,71 +25,71 @@ public class Laps {
     }
 
 
-
-
     public int getAHR() {
         if (avgHR != null) {
             return avgHR;
         }
-        public LocalDateTime getStartTime () {
-            if (tracks.isEmpty()) return null;
-            return tracks.get(0).getStartTime();
-        }
+    }
 
-
-
-        public double getDistanceMeters () {
-            return tracks.stream()
-                    .mapToDouble(Tracks::getDistanceMeters)
-                    .sum();
-        }
-
-        public double getAverageSpeed () {
-            double t = getTimeSeconds();
-            if (t == 0) return 0;
-            return getDistanceMeters() / t;
-        }
-
-
-        public int getMHR () {
-            return tracks.stream()
-                    .mapToInt(Tracks::getMHR)
-                    .max()
-                    .orElse(0);
-        }
-        //SETTERS
-        public void setTimeSeconds ( double t){
-            this.totalTimeSeconds = t;
-        }
-
-        public void setAHR ( int hr){
-            this.avgHR = hr;
-        }
-
-
-        public void setDistanceMeters(Double distanceMeters) {
-            this.distanceMeters = distanceMeters;
-        }
-
-        public void setMaxHR(int maxHR) {
-            this.maxHR = maxHR;
-        }
-
-        public void setAveragespeed(double averagespeed) {
-            this.averagespeed = averagespeed;
-        }
-
-        public void setTracks(List<Tracks> tracks) {
-            this.tracks = tracks;
-        }
-        public void setStartTime(LocalDateTime startTime) {
-            StartTime = startTime;
-        }
-
-        public List<Tracks> getTracksList () {
-            return tracks;
-        }
+    public LocalDateTime getStartTime() {
+        if (tracks.isEmpty()) return null;
+        return tracks.get(0).getStartTime();
     }
 
 
+    public double getDistanceMeters() {
+        return tracks.stream()
+                .mapToDouble(Tracks::getDistanceMeters)
+                .sum();
+    }
+
+    public double getAverageSpeed() {
+        double t = getTimeSeconds();
+        if (t == 0) return 0;
+        return getDistanceMeters() / t;
+    }
+
+
+    public int getMHR() {
+        return tracks.stream()
+                .mapToInt(Tracks::getMHR)
+                .max()
+                .orElse(0);
+    }
+
+    //SETTERS
+    public void setTimeSeconds(double t) {
+        this.totalTimeSeconds = t;
+    }
+
+    public void setAHR(int hr) {
+        this.avgHR = hr;
+    }
+
+
+    public void setDistanceMeters(Double distanceMeters) {
+        this.distanceMeters = distanceMeters;
+    }
+
+    public void setMaxHR(int maxHR) {
+        this.maxHR = maxHR;
+    }
+
+    public void setAveragespeed(double averagespeed) {
+        this.averagespeed = averagespeed;
+    }
+
+    public void setTracks(List<Tracks> tracks) {
+        this.tracks = tracks;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        StartTime = startTime;
+    }
+
+    public List<Tracks> getTracksList() {
+        return tracks;
+    }
 }
+
+
